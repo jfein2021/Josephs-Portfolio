b3d37d1fcf7791dba9772dce7a418c9a43587e80
jfein2021 <jfein2021@gmail.com>
2026-07-17 04:57:06 -0400

    Add files via upload

--- navbar.js
+++ navbar.js
@@ -1,5 +1,19 @@
+// 1. Fetch and inject the Navbar
 fetch('navbar.html')
     .then(response => response.text())
     .then(data => {
-        document.getElementById('navbar-placeholder').innerHTML = data;
+        const navbarPlaceholder = document.getElementById('navbar-placeholder');
+        if (navbarPlaceholder) {
+            navbarPlaceholder.innerHTML = data;
+        }
+    });
+
+// 2. Fetch and inject the Contact Footer
+fetch('footer.html')
+    .then(response => response.text())
+    .then(data => {
+        const footerPlaceholder = document.getElementById('footer-placeholder');
+        if (footerPlaceholder) {
+            footerPlaceholder.innerHTML = data;
+        }
     });
